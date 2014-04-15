4e7c85c742323b1030924f1e1e14ced3a2305cfe
Lukas <lukas@brown.edu>
2014-04-14 23:02:22 -0400

    Fiddlin' with wrapper structure mostly.

--- print.js
+++ print.js
@@ -2,14 +2,18 @@ $('document').ready(function(){
 
   // The gist is to make a print-ready CSS file that gets slipped into the head.
 
-  // 1cm = 118 pixels; 1cm = 28.3 points; 1cm = 2.36 picas
   // Lukas WinklerPrins
   // Updated 14 April 2014
 
-  var p_width = 11;
-  var p_length = 17;
+  // 1in = 6pc = 72pt
+  // 1cm = 2.36pc = 28.3pt
+
+  // Damn dog what kind of resolution we wanna print at? aka pushing pixel units to inches
+
+  var p_width = 4;
+  var p_length = 6;
   var p_unit = "in";
-  // 8.5x11 is 51x66
+
 
   var p_m = "";
   // this will be the string we add all styling to.
@@ -18,13 +22,16 @@ $('document').ready(function(){
   // Check for a container/wrapper of the entire document
   var wrap_name = $("body > div").eq(0).attr('id');
   var wrap_identifier = "#";
+  var is_wrap;
   if(wrap_name == null){
     wrap_name = $("body > div").eq(0).attr('class');
     wrap_identifier = ".";
+    is_wrap = 1;
   }
   if(wrap_name != 'container' && wrap_name != 'wrapper'){
     wrap_name = "body";
     wrap_identifier = "";
+    is_wrap = 0;
   }
 
   var num_cont = $(wrap_identifier+wrap_name+" > div").length;
@@ -52,7 +59,7 @@ $('document').ready(function(){
       }
     }
 
-    alert($(cont_identifier+cont_name).css("height")); // returns stuff as "##px" thing
+    //alert($(cont_identifier+cont_name).css("height")); // returns stuff as "##px" thing
 
     // assumes all lil repeated nuggets are just divs or share the same class
     var num_repeat = 0;
@@ -70,13 +77,22 @@ $('document').ready(function(){
 
     //////////////////////////////////////////////////////
 
-    var cont_css = "{width: "+p_width+p_unit+"; height: "+p_length+p_unit+";} ";
-    var repeat_css = "{page-break-after: always;} ";
+    var cont_css = "{ ";
+    if(!is_wrap){
+      cont_css = cont_css + "width: "+p_width+p_unit+"; height: "+p_length+p_unit+"; overflow: hidden;";
+      // if there is no wrapper on which to hide overflow, hide it at the container level.
+    }
+    cont_css = cont_css + "} ";
+
+    var repeat_css = "{} ";
+
     p_m = p_m + cont_identifier + cont_name + cont_css;
+
     if(repeat_name == null){
       p_m = p_m + cont_identifier+cont_name+" > div" + repeat_css;
     }else{
       p_m = p_m + "."+repeat_name+repeat_css;
+      // must be a class, not ID
     }
 
   }
@@ -84,7 +100,9 @@ $('document').ready(function(){
   // end the containers loop
 
   p_m = p_m + "body{background-color: transparent; overflow: hidden; } ";
-  p_m = p_m + wrap_name+"{overflow: hidden; width: 4in; } ";
+
+  var wrap_css = "{overflow: hidden; width: "+p_width+p_unit+"; height: "+p_length+p_unit+";} ";
+  p_m = p_m + wrap_identifier+wrap_name+wrap_css;
 
   /*
   var crop_marks1 = "#mark1{position: fixed; top: 0"+p_unit+"; left: 0"+p_unit+"; width: 1px; height: 30px; background-color: black;} #mark2{position: fixed; top: 0"+p_unit+"; right: "+p_width+p_unit+"; width: 30px; height: 1px; background-color: black;} #mark3{position: fixed; bottom: "+p_length+p_unit+"; left: 0"+p_unit+"; width: 1px; height: 30px; background-color: black;} #mark4{position: fixed; bottom: "+p_length+p_unit+"; right: "+p_width+p_unit+"; width: 1px; height: 30px; background-color: black;}";
@@ -121,22 +139,25 @@ $('document').ready(function(){
 // TO ADD: ///////////////////////////////////////////////////////////////////////
 // CHECK looping for multiple containers
 // CHECK container wrapping—fix to 11x17 size, now via body
-// options for "chunking" vs "overlapping"
 // CHECK double check .html() command
 // CHECK (kinda) convert to picas/points...
-// >center the material in the page ?
 // CHECK accomodate for no existing <style> tag, i.e. external stylesheet
-// >span, h1, h2, h3 ... cases instead of div
 // NOPE turning images off?
 // CHECK collab with cathland
 // NOPE adjust print output to browser window size ???
 // CHECK multiple media formats
 // CHECK look for total wrapper
-// >do something with # of repeats (page structuring ...?)
 // UH crop marks ???
+//
+// options for "chunking" vs "overlapping" ?
+// span, h1, h2, h3 ... cases instead of div ?
+// center the material in the page
+// do something with # of repeats (page structuring ...?)
+// accommodate for margins of printed page when setting size
+// add wrapper no matter what...?
 
 
-// Some notes:
+// SOME NOTES:
 // > All javascript must be in the head or external file. None in the body. Use jquery!
 // > Then put print.js at the very end of your head.
 // > The outside thing should be called either "wrapper" or "container" otherwise it will assume it is a structural div rather than big old wrapper thingy
